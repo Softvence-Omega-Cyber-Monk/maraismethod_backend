1a4763bc2067dc4389e03095c81c087315c77d71
shahadathhs <shahadathhossensajib732@gmail.com>
2025-12-24 11:11:37 +0600

    feat: admin analytics compeleted

--- src/main/statistics/statistics.service.ts
+++ src/main/statistics/statistics.service.ts
@@ -2,27 +2,28 @@ import { successResponse, TResponse } from '@/common/utils/response.util';
 import { HandleError } from '@/core/error/handle-error.decorator';
 import { PrismaService } from '@/lib/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { DateTime } from 'luxon';
 
 @Injectable()
 export class StatisticsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  @HandleError("Failed to fetch analytics")
+  @HandleError('Failed to fetch analytics')
   async getAnalytics(): Promise<TResponse<any>> {
     const [totalVenues, categories, totalUsers, activeUsers, totalVotes] =
       await this.prisma.client.$transaction([
         this.prisma.client.venue.count(),
         this.prisma.client.venue.groupBy({
-  by: ['catgegory'], // matches the model
-  _count: {
-    catgegory: true,
-  },
-  orderBy: {
-    _count: {
-      catgegory: 'desc',
-    },
-  },
-}),
+          by: ['catgegory'], // matches the model
+          _count: {
+            catgegory: true,
+          },
+          orderBy: {
+            _count: {
+              catgegory: 'desc',
+            },
+          },
+        }),
         this.prisma.client.user.count(),
         this.prisma.client.user.count({
           where: { status: 'ACTIVE' },
@@ -45,7 +46,7 @@ export class StatisticsService {
     );
   }
 
-  @HandleError("Failed to fetch recent venues")
+  @HandleError('Failed to fetch recent venues')
   async getRecentVenues(): Promise<TResponse<any>> {
     const venues = await this.prisma.client.venue.findMany({
       take: 5,
@@ -65,7 +66,7 @@ export class StatisticsService {
     return successResponse(venues, 'Recent venues fetched successfully');
   }
 
-  @HandleError("Failed to fetch recent votes")
+  @HandleError('Failed to fetch recent votes')
   async getRecentVotes(): Promise<TResponse<any>> {
     const votes = await this.prisma.client.votes.findMany({
       take: 5,
@@ -87,6 +88,21 @@ export class StatisticsService {
       },
     });
 
-    return successResponse(votes, 'Recent votes fetched successfully');
+    const recentActivities = votes.map((v) => {
+      const timeAgo = DateTime.fromJSDate(v.createdAt).toRelative({
+        locale: 'en',
+      });
+      return {
+        userName: v.user.name,
+        action: `voted ${v.isOpen ? 'open' : 'closed'}`,
+        venueName: v.venue.name,
+        timeAgo,
+      };
+    });
+
+    return successResponse(
+      recentActivities,
+      'Recent votes fetched successfully',
+    );
   }
 }
